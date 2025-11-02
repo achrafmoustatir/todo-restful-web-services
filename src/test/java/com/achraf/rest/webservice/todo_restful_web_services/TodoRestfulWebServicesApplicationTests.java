6c8da1de566caf8b461a2f8dfb8e6b7d16fce701
achrafmoustatir <achraf.moustatir@gmail.com>
2025-11-02 21:56:15 +0100

    Initial commit

---
+++ src/test/java/com/achraf/rest/webservice/todo_restful_web_services/TodoRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achraf.rest.webservice.todo_restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
